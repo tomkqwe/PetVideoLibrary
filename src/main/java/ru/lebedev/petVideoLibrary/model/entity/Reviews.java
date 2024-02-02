08e8680ec09f92fd4967abcae0af25e017be2738
tomkqwe <tomkqwe@gmail.com>
2024-02-02 23:29:25 +0300

    Add liquibase and connect postgres

--- src/main/java/ru/lebedev/petVideoLibrary/model/entity/Reviews.java
+++ src/main/java/ru/lebedev/petVideoLibrary/model/entity/Reviews.java
@@ -3,18 +3,18 @@ package ru.lebedev.petVideoLibrary.model.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import ru.lebedev.petVideoLibrary.model.enums.Grade;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Table(name = "reviews")
 public class Reviews {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Film film;
-    private User user;
+//    private Film film;
+//    private User user;
     private String description;
     @Enumerated(EnumType.STRING)
     private Grade grade;
